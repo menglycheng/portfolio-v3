08af09554ceed0bb4d93c992c791c6925fe93d49
menglycheng <cheng.mengly19@kit.edu.kh>
2022-11-26 23:19:35 +0700

    Fix error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    externalDir: true,
+  },
+  disableExperimentalFeaturesWarning: true,
   reactStrictMode: true,
-}
+};
