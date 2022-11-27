879d99d9783b7e3761b20585a47ad33b0e162a46
menglycheng <cheng.mengly19@kit.edu.kh>
2022-11-27 10:31:10 +0700

    Fix error

--- components/Navbar.js
+++ components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
                     : ""
                 }`}
               >
-                {item.label}
+                <a>{item.label}</a>
               </Link>
             </div>
           );
