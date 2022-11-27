d410711a53aefba82325230849a363622061f8b0
menglycheng <cheng.mengly19@kit.edu.kh>
2022-11-27 11:08:17 +0700

    Fix error

--- utils/fetchCompetition.js
+++ utils/fetchCompetition.js
@@ -1,6 +1,6 @@
 export const fetchCompetition = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}api/getCompetition`
+    `https://portfolio-v3-diduybptw-mengly19.vercel.app/api/getCompetition`
   );
   const competition = await res.json();
 
