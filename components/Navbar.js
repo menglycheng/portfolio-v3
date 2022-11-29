1112488d4e39479086ed4ad044bd1d97853af39b
menglycheng <cheng.mengly19@kit.edu.kh>
2022-11-29 17:45:43 +0700

    add Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,20 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import MenuItems from "../utils/menuItems";
 import { useRouter } from "next/router";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
-import { Bars3Icon } from "@heroicons/react/24/outline";
-
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, Transition } from "@headlessui/react";
 const Navbar = () => {
   const router = useRouter();
   const [greeting, setGreeting] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   const [text, count] = useTypewriter({
     words: greeting,
     loop: true,
     delaySpeed: 2000,
   });
-
   useEffect(() => {
     const time = new Date().getHours();
     if (time >= 1 && time <= 12) {
@@ -32,8 +32,53 @@ const Navbar = () => {
           {text} <Cursor cursorColor="white" />
         </span>
       </h2>
-      <div>
-        <div className="flex flex-col justify-end text-right md:flex-row   ">
+      {/* window screen */}
+      <div className=" md:hidden flex flex-col">
+        <div className="w-full  flex justify-end px-3 ">
+          {isOpen ? (
+            <XMarkIcon className="w-6 h-6 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
+          ) : (
+            <Bars3Icon className="w-6 h-6 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
+          )}
+        </div>
+        <Transition
+          show={isOpen}
+          as={Fragment}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+          onClose={setIsOpen}
+        >
+          {(ref) => (
+            <div
+              ref={ref}
+              className="flex  justify-end text-right flex-col mt-5  "
+            >
+              {MenuItems.map((item, index) => {
+                return (
+                  <div key={item.id} className="my-1 md:my-0">
+                    <Link
+                      href={item.url}
+                      className={`px-3  ${
+                        router.asPath === item.url
+                          ? " underline decoration-wavy text-blue-500 "
+                          : ""
+                      }`}
+                    >
+                      {item.label}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </Transition>
+      </div>
+      <div className="hidden md:flex">
+        <div className="flex  justify-end text-right md:flex-row   ">
           {MenuItems.map((item, index) => {
             return (
               <div key={item.id} className="my-1 md:my-0">
